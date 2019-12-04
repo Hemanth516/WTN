7656331a0f021d05472fba0828b93b5a474a7ab2
Hemanth516 <akulahemanth516@gmail.com>
2019-12-04 10:31:03 +0530

    Add files via upload

---
+++ P1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class P1
+
+{
+
+ public static void main(String args[])
+ {
+
+   System.out.println(args[0]+" Technologies "+args[1]);
+ 
+}
+
+}
---
+++ P5.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class P5
+  
+{
+  
+ public static void main(String args[])
+     
+{
+     
+ Scanner sc=new Scanner(System.in);
+     
+int n=sc.nextInt();
+      
+if(n%2==0)
+       
+System.out.println("The number "+n+" is even.");
+      
+      
+else
+         
+System.out.println("The number "+n+" is odd.");
+     
+}
+  
+}
+
